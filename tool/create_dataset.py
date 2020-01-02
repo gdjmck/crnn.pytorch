6589e0a948c7c49b3b684b3e0ee042ade8f86ff4
chenkang <lv23ck@hotmail.com>
2020-01-02 17:50:29 +0800

    stash local changes to be merged

---
+++ tool/create_dataset.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+import os
+import lmdb # install lmdb by "pip install lmdb"
+import cv2
+import numpy as np
+import glob
+
+
+def checkImageIsValid(imageBin):
+    if imageBin is None:
+        return False
+    imageBuf = np.fromstring(imageBin, dtype=np.uint8)
+    img = cv2.imdecode(imageBuf, cv2.IMREAD_GRAYSCALE)
+    imgH, imgW = img.shape[0], img.shape[1]
+    if imgH * imgW == 0:
+        return False
+    return True
+
+
+def writeCache(env, cache):
+    with env.begin(write=True) as txn:
+        for k, v in cache.iteritems():
+            txn.put(k, v)
+
+
+def createDataset(outputPath, imagePathList, labelList, lexiconList=None, checkValid=True):
+    """
+    Create LMDB dataset for CRNN training.
+    ARGS:
+        outputPath    : LMDB output path
+        imagePathList : list of image path
+        labelList     : list of corresponding groundtruth texts
+        lexiconList   : (optional) list of lexicon lists
+        checkValid    : if true, check the validity of every image
+    """
+    assert(len(imagePathList) == len(labelList))
+    nSamples = len(imagePathList)
+    print('number of samples:', nSamples)
+    env = lmdb.open(outputPath, map_size=1099511627776)
+    cache = {}
+    cnt = 1
+    for i in xrange(nSamples):
+        imagePath = imagePathList[i]
+        label = labelList[i]
+        if not os.path.exists(imagePath):
+            print('%s does not exist' % imagePath)
+            continue
+        with open(imagePath, 'rb') as f:
+            imageBin = f.read()
+        if checkValid:
+            if not checkImageIsValid(imageBin):
+                print('%s is not a valid image' % imagePath)
+                continue
+
+        imageKey = 'image-%09d' % cnt
+        labelKey = 'label-%09d' % cnt
+        cache[imageKey] = imageBin
+        cache[labelKey] = label
+        if lexiconList:
+            lexiconKey = 'lexicon-%09d' % cnt
+            cache[lexiconKey] = ' '.join(lexiconList[i])
+        if cnt % 1000 == 0:
+            writeCache(env, cache)
+            cache = {}
+            print('Written %d / %d' % (cnt, nSamples))
+        cnt += 1
+    nSamples = cnt-1
+    cache['num-samples'] = str(nSamples)
+    writeCache(env, cache)
+    print('Created dataset with %d samples' % nSamples)
+
+provinces = ["皖", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏", "浙", "京", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂", "琼", "川", "贵", "云", "藏", "陕", "甘", "青", "宁", "新", "警", "学", "O"]
+alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+             'X', 'Y', 'Z', 'O']
+ads = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+       'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O']
+
+def interpret_plate_name(fn):
+    chars = fn.rsplit('/', 1)[-1].split('-')[-3].split('_')
+    plate = []
+    for i, char in enumerate(chars):
+        char_idx = int(char)
+        if i == 0:
+            plate.append(provinces[char_idx])
+        elif i == 1:
+            plate.append(alphabets[char_idx])
+        else:
+            plate.append(ads[char_idx])
+    return ''.join(plate)
+
+def prepare(train_folder):
+    files = glob.glob(os.path.join(train_folder, '*'))
+    imagePathList, labelList = [], []
+    for file in files:
+        car_plate = interpret_plate_name(file)
+        imagePathList.append(file)
+        labelList.append(car_plate)
+    assert len(imagePathList) == len(labelList)
+    return imagePathList, labelList
+
+
+if __name__ == '__main__':
+    imagePathList, labelList = prepare('/home/chk/Documents/data/AIMakeup/tilted_plates')
+    createDataset('/home/chk/Documents/ai-park/crnn.pytorch/lmdb', 
+                    imagePathList, labelList)
